92d2a31825e41e322e4ec46b4ca3294fa9f947f2
kim-0617 <kkk5993@naver.com>
2023-05-07 18:18:18 +0900

    Create Hero Section
    프로젝트 설명 섹션 추가

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { logo } from '../assets/index';
 
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="w-full mb-10 pt-3 flex justify-between items-center">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open('')}
+          className="black_btn"
+        >
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Search the url and it will give you a summary of the article. <br />{' '}
+        There may be some invalid urls.
+      </h2>
+    </header>
+  );
 }
 
 export default Hero;
