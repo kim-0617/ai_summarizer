93a2f05b7b165d3f09a1961b78570699927fc4b7
kim-0617 <kkk5993@naver.com>
2023-05-07 15:52:18 +0900

    Initialize vite project
    프로젝트 초기셋팅

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <div className="main">
+    <div className="gradient" />
+      </div>
+    </main>
+  );
+}
+
+export default App;
