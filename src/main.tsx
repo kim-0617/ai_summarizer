72f2d51959c9675bc181259a9bde744d177af8e6
kim-0617 <kkk5993@naver.com>
2023-05-07 18:17:53 +0900

    Connect redux store

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { Provider } from 'react-redux';
+import { store } from './services/store.ts';
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
 );
